b7154ea6470ed52f0957d3c78f1268872f83998b
emanuelvallejoc <emanuel.vallejo@sofka.com.co>
2022-07-10 23:26:10 -0500

    solucion actividad 2

--- src/main/java/io/pivotal/literx/Part04Transform.java
+++ src/main/java/io/pivotal/literx/Part04Transform.java
@@ -13,27 +13,42 @@ public class Part04Transform {
 
 //========================================================================================
 
+
+
 	// TODO Capitalize the user username, firstname and lastname
 	Mono<User> capitalizeOne(Mono<User> mono) {
-		return null;
+		return mono.map(this::capitalize);
 	}
 
 //========================================================================================
 
 	// TODO Capitalize the users username, firstName and lastName
 	Flux<User> capitalizeMany(Flux<User> flux) {
-		return null;
+
+		return flux.map(this::capitalize);
 	}
 
 //========================================================================================
 
 	// TODO Capitalize the users username, firstName and lastName using #asyncCapitalizeUser
 	Flux<User> asyncCapitalizeMany(Flux<User> flux) {
-		return null;
+		return flux.flatMap(this::asyncCapitalizeUser);
 	}
 
 	Mono<User> asyncCapitalizeUser(User u) {
 		return Mono.just(new User(u.getUsername().toUpperCase(), u.getFirstname().toUpperCase(), u.getLastname().toUpperCase()));
+	}
+
+	/**
+	 *
+	 * @param user
+	 * @return nuevo usuario para disminuor codigo repetido se crea sync capitaliza
+	 */
+
+	private User capitalize(User user){
+		return new User(user.getUsername().toUpperCase(),
+				user.getFirstname().toUpperCase(),
+				user.getLastname().toUpperCase());
 	}
 
 }
